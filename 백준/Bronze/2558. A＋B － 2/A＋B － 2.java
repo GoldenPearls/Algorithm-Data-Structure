9632729c92055b8aed8f4325185c060a1f2adc2d
Geυмjυ lee <97003348+GoldenPearls@users.noreply.github.com>
2024-04-02 16:34:19 +0900

    [Bronze V] Title: A+B - 2, Time: 208 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        
+        int num1, num2;
+        num1 = in.nextInt();
+        num2 = in.nextInt();
+        
+        System.out.print(num1+num2);
+    }
+}
